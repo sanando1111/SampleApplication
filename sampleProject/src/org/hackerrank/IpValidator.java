498574a5a8f94a470913adefa7917520a06154e5
Wolverine <Wolverine@DESKTOP-942HOHM>
2020-04-15 00:03:42 +0530

    Added Ip Validator class
    Change-Id: Ia314883ca00a79c74bceb4511b11b475c68cf580

--- sampleProject/src/org/hackerrank/IpValidator.java
+++ sampleProject/src/org/hackerrank/IpValidator.java
@@ -1,5 +1,35 @@
 package org.hackerrank;
 
+import java.util.Arrays;
+
 public class IpValidator {
+
+	public static void main(String[] args) {
+
+		String ip = "I.Am.not.an+ip";
+		if (ip.chars().filter(s -> s == '.').count() == 3) {
+			String[] ipar = ip.split("\\.");
+			for (String i : ipar)
+				System.out.println(i);
+			if (ipar.length != 4)
+				System.out.println("Invalid Ip");
+			else {
+				try {
+					long p = Arrays.stream(ipar).filter(s -> s.length() >= 2).map(Integer::parseInt)
+							.filter(i -> (i >= 0 && i <= 255)).count();
+					System.out.println(p == 4 ? "Valid Ip" : "Invalid Ip");
+				}
+				catch(Exception e)
+				{
+					System.out.println("Invalid Ip");
+				}
+
+				
+			}
+		} else {
+			System.out.println("Invalid Ip");
+		}
+
+	}
 
 }
